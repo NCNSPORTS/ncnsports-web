a786640a2667fe78391c700d3cb14f14f72a12e2
Gabriel Martim <gmartim@clb.santillana.com>
2026-02-06 00:20:31 -0300

    Implementacao styled-components

---
+++ src/app/components/home/Title.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+
+  .divdoidona {
+    width: 100%;
+    height: 100px;
+    background-color: blue;
+  }
+
+`;
+
+export default Title; 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 "use client";
+import React from 'react';
+import Title from './components/home/Title';
 
 const Page = () => {
   return (
     <>
-      <h1>Welcome to the Home Page</h1>
+      <Title>
+        <ul>
+          <li className="li">Welcome to NCN Sports!</li>
+          <li className="li">Your hub for all sports news.</li>
+          <li className="li">Stay tuned for updates.</li>
+        </ul>
+
+        <div className='divdoidona'>asdf</div>
+      </Title>
     </>
   );
 }
