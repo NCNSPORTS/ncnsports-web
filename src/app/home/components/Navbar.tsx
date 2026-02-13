7c5d8f35c27bb9c12d865ec3880743e8536c0460
Gabriel Martim <gmartim@clb.santillana.com>
2026-02-13 09:34:29 -0300

    add divider component & api

--- src/app/home/components/Navbar.tsx
+++ src/app/home/components/Navbar.tsx
@@ -4,7 +4,6 @@ export const Navbar = styled.div`
     display: flex;
     width: 100%;
     height: 100%;
-    border-top: 1px solid var(--color-hover);
     > a {
      margin: 0 10px;
      padding: 10px 15px;
